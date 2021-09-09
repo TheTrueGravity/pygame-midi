21f01e6ef5d4604b41bce0a727e2084d71173182
Charlie <75177875+TheTrueGravity@users.noreply.github.com>
2021-09-09 17:38:02 +0100

    main

--- python/communication.py
+++ python/communication.py
@@ -7,25 +7,44 @@ midi.init()
 
 if (not midi.get_init()):
     exit(1)
+    
+def get_device_info(id: int):
+    if (midi.get_device_info(id) == None):
+        return "invalid_device_id"
 
-interface, name, input, output, opened = midi.get_device_info(int(sys.argv[1]))
+    interface, name, input, output, opened = midi.get_device_info(id)
 
-name = str(name).replace("b", "").replace("'", "")
-interface = str(interface).replace("b", "").replace("'", "")
+    name = str(name).replace("b", "").replace("'", "")
+    interface = str(interface).replace("b", "").replace("'", "")
 
-type = "Input"
-if (output == 1): type = "Output"
-if (opened == 0): opened = False
-else: opened = True
+    type = "Input"
+    if (output == 1): type = "Output"
+    if (opened == 0): opened = False
+    else: opened = True
 
-output = {
-    "port": int(sys.argv[1]),
-    "interface": interface,
-    "name": name,
-    "type": type,
-    "opened": opened
-}
+    output = {
+        "port": id,
+        "interface": interface,
+        "name": name,
+        "type": type,
+        "opened": opened
+    }
 
-print(json.dumps(output))
+    return output
+
+if (sys.argv[1] == "get_device_info"):
+    device_id = int(sys.argv[2])
+    device_info = get_device_info(device_id)
+    print(json.dumps(device_info))
+
+elif (sys.argv[1] == "get_all_devices"):
+    port_count = midi.get_count()
+    output = []
+    for i in range(port_count):
+        output.append(get_device_info(i))
+    print(json.dumps(output))
+
+elif (sys.argv[1] == "get_device_count"):
+    print(midi.get_count())
 
 midi.quit()
